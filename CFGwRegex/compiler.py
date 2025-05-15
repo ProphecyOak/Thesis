42a89066733008d409cf7a325ac8b24e7cebc23b
ProphecyOak <drito610@gmail.com>
2025-05-15 13:22:46 -0400

    Remove all the Unnecessary Old Versions

--- CFGwRegex/compiler.py
+++
@@ -1,9 +0,0 @@
-from gram import gramString
-from parser import parseProgram
-from evaluator import Evaluator
-
-with open("CFGwRegex/testProgram.txt", "r") as f:
-	codeLines = parseProgram(f.read(), gramString)
-	# for line in codeLines: print(line)
-	myEvaluator = Evaluator(codeLines)
-	myEvaluator.evaluateProgram()
--- TypeScript 5 - EndGame/src/executive/shellLex.ts
+++
@@ -1,4 +0,0 @@
-import { SymbolTable, testTable } from "../components/lexicon";
-export { lexicon };
-
-const lexicon = new SymbolTable(testTable);
